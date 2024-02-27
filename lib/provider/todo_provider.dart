ee242a15fb1723eadc6d20c6f2e2c29b93848f67
Rabin Phaiju <rabinphaiju15@gmail.com>
2024-02-27 09:24:11 +0545

    update: todo ui.

--- lib/provider/todo_provider.dart
+++ lib/provider/todo_provider.dart
@@ -43,42 +43,53 @@ class TodoModelNotifier extends AutoDisposeNotifier<List<TodoModel>> {
     state = [todo,...state];
   }
 
- void remove(TodoModel todo) {
-    state = state.where((element) => element.id != todo.id).toList();
+ void remove(int id) {
+    state = state.where((element) => element.id != id).toList();
   }
 
-  void update(TodoModel todo) {
-    state = state.map((e) => e.id == todo.id ? todo : e).toList();
+  void update(int id, String description) {
+    state = state.map((e) => e.id == id ? e.copyWith(description: description) : e).toList();
   }
 
+  void toggle(int id) {
+    state = state.map((e) => e.id == id ? e.copyWith(isComplete: !e.isComplete) : e).toList();
+  }
 
   void clear() {
     state = [];
   }
 }
 
-// expose stateNotifier
+// expose Notifier
+// here as a `List<TodoModel>` is a complex object, with advanced business logic like how to edit a todo.
 final todoProvider = NotifierProvider.autoDispose<TodoModelNotifier, List<TodoModel>>(TodoModelNotifier.new);
 
+/// This ensures that when we add/remove/edit todos, only what the
+/// impacted widgets rebuilds, instead of the entire list of items.
+final currentTodo = Provider<TodoModel>((ref) => throw UnimplementedError());
 
-// class TodoModelNotifier extends StateNotifier<List<TodoModel>> {
-//   // TodoModelNotifier(super.state);
-//   TodoModelNotifier() : super([]);
-//
-//   void addTodo(TodoModel todo) {
-//     state = [todo,...state];
-//   }
-//
-//  void removeTodo(TodoModel todo) {
-//     state = state.where((element) => element.id != todo.id).toList();
-//   }
-//
-//   void clear() {
-//     state = [];
-//   }
-// }
-//
-// // expose stateNotifier
-// final todoProvider = StateNotifierProvider.autoDispose<TodoModelNotifier, List<TodoModel>>((ref) {
-//   return TodoModelNotifier();
-// });
+enum TodoListFilter { all, active, completed, }
+/// here is no fancy logic behind manipulating the value since it's just enum.
+final todoListFilter = StateProvider.autoDispose((_) => TodoListFilter.all);
+
+/// By using [Provider], this value is cached, making it performant.Even multiple widgets try to read the number of uncompleted todos,
+/// the value will be computed only once (until the todo-list changes).
+/// This will also optimise unneeded rebuilds if the todo-list changes, but the number of uncompleted todos doesn't (such as when editing a todo).
+final uncompletedTodosCount = Provider.autoDispose<int>((ref) {
+  return ref.watch(todoProvider).where((todo) => !todo.isComplete).length;
+});
+
+/// This too uses [Provider], to avoid recomputing the filtered list unless either the filter of or the todo-list updates.
+final filteredTodos = Provider.autoDispose<List<TodoModel>>((ref) {
+  final filter = ref.watch(todoListFilter); // if used watcher, it will be recomputed when todoListFilter changes.
+  final todos = ref.watch(todoProvider); // if used watcher, it will be recomputed when todoList changes.
+
+  switch (filter) {
+    case TodoListFilter.completed:
+      return todos.where((todo) => todo.isComplete).toList();
+    case TodoListFilter.active:
+      return todos.where((todo) => !todo.isComplete).toList();
+    case TodoListFilter.all:
+      return todos;
+  }
+});
--- lib/screens/todo_provider.dart
+++ lib/screens/todo_provider.dart
@@ -3,66 +3,75 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_learn/widgets/add_todo.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import '../provider/todo_provider.dart';
+import '../widgets/todo_item.dart';
+import '../widgets/todo_toolbar.dart';
 
-class TodoStateNotifierWidget extends ConsumerWidget {
+class TodoStateNotifierWidget extends HookConsumerWidget {
   const TodoStateNotifierWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userList = ref.watch(todoProvider);
-    // final user = ref.watch(userProvider.select((value) => value.age)); // only watch the age.
+    // final todoList = ref.watch(userProvider.select((value) => value.age)); // only watch the age.
+    // final todoList = ref.watch(todoProvider);
+    final todoList = ref.watch(filteredTodos);
+    final newTodoController = useTextEditingController();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('State Notifier Provider'),
+        title: const Text('Notifier Provider'),
       ),
-      body: Column(
-        children: [
-          userList.isEmpty
-              ? const Center(
-                child: Text('No Data'),
-              ):
-              Expanded(
-                child: ListView.builder(
-                  itemCount: userList.length,
-                    itemBuilder: (context, index) {
-                      final todo = userList[index];
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(todo.title,),
-                                  Text(todo.description),
-                                  Text(todo.dateTime.toString()),
-                                ]
-                              ),
-                              TextButton(
-                                  onPressed: (){
-                                    ref.read(todoProvider.notifier).remove(todo);
-                                  },
-                                  child: const Icon(Icons.delete))
-                            ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Toolbar(),
+            todoList.isEmpty
+              ? TextField(
+                  controller: newTodoController,
+                  decoration: const InputDecoration(
+                    labelText: 'What needs to be done?',
+                  ),
+                  onSubmitted: (value) {
+                    final title = value;
+                    final description = value;
+                    if(title.isEmpty || description.isEmpty){ return;}
+                    final id = Random.secure().nextInt(100)+1;
+                    ref.read(todoProvider.notifier).add(TodoModel(title, description, false, DateTime.now(), id));
+                    newTodoController.clear();
+                  },
+                )
+                : Expanded(
+                  child: ListView.builder(
+                    itemCount: todoList.length,
+                      itemBuilder: (context, index) {
+                        final todo = todoList[index];
+                        return  Dismissible(
+                          key: ValueKey(todo.id),
+                          onDismissed: (_) {
+                            ref.read(todoProvider.notifier).remove(todo.id);
+                          },
+                          child: ProviderScope(
+                            overrides: [ currentTodo.overrideWithValue(todo), ],
+                            child: const TodoItem(),
                           ),
-                        ),
-                      );
-                    }
+                        );
+                      }
+                  ),
                 ),
-              ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => getDddTodoSheet(context),
+        onPressed: () => getTodoSheet(context),
         child: const Icon(Icons.add),
       )
     );
   }
 
-  void getDddTodoSheet(BuildContext context){
+  void getTodoSheet(BuildContext context){
     showModalBottomSheet(
         useSafeArea: true,
         showDragHandle: true,
